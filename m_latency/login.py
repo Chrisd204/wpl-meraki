cc84cf6618224274f6d9ff0bb1f7988869073869
Chrisd204 <chrisdurhamjr@gmail.com>
2020-08-05 15:33:41 -0400

    created test file for refactoring

---
+++ m_latency/login.py
@@ -0,0 +1,9 @@
+api_key = "1d065cd1f102683fabbe27a5b5ee669602e305c0"
+org_id = '618032'
+
+lab_email = 'labupdates008@gmail.com'
+lab_email_password = 'mm"54TQ@d/K[C&GH]&5'
+company_email = 'christopher.durham@comop.org'
+
+smtp_port = 587
+smtp_server = "smtp.gmail.com"
