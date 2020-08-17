b623e864c943e9ed3e2c02eaa19eb2b05aa25c93
Chrisd204 <chrisdurhamjr@gmail.com>
2020-08-17 09:19:24 -0400

    updated to match production code.

--- m_latency/login.py
+++ m_latency/login.py
@@ -3,7 +3,12 @@ org_id = '618032'
 
 lab_email = 'labupdates008@gmail.com'
 lab_email_password = 'mm"54TQ@d/K[C&GH]&5'
-company_email = 'christopher.durham@comop.org'
+company_email = 'Meraki_Alerts@comop.org'
+
+my_email = 'Christopher.Durham@comop.org'
+
+monitor_email = 'comop.monitorbox@gmail.com'
+monitor_email_password = 'eKTmE&yM91tIJ6mIGVwl!CM#HZxebRjehTvf7s2Da'
 
 smtp_port = 587
 smtp_server = "smtp.gmail.com"
