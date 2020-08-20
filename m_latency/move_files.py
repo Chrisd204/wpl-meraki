cddc357b4fea6d0f554ed52d60e7286e50aa0905
Chrisd204 <chrisdurhamjr@gmail.com>
2020-08-20 10:35:20 -0400

    updated path for test environment.. will need to update move file script to account for each system.

--- m_latency/move_files.py
+++ m_latency/move_files.py
@@ -1,5 +1,5 @@
 import os
 
 # ------ move files to archive folder
-os.system('mv ~/wpl*.xlsx ~/wpl-Meraki/wpl-Archive/dpl')
-os.system('mv ~/averages*.xlsx ~/wpl-Meraki/wpl-Archive/averages')
+os.system('mv ~/wpl*.xlsx ~/Documents/wpl-archive/dpl')
+os.system('mv ~/averages*.xlsx ~/Documents/wpl-archive/averages')
